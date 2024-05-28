d6ae84696c97c6beb9edaff318ccee9eaf52f59d
Harish Mohan Raj <harish@airt.ai>
2024-05-28 20:03:41 +0530

    Copy template files

---
+++ app/src/client/components/SelectTeamToChat.tsx
@@ -0,0 +1,11 @@
+const SelectTeamToChat = () => {
+  return (
+    <p
+      className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl md:text-6xl text-captn-light-cream opacity-70'
+      style={{ lineHeight: 'normal' }}
+    >
+      Please initiate a new chat or select existing chats to resume your conversation.
+    </p>
+  );
+};
+export default SelectTeamToChat;
