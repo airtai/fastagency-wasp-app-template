c4ad07177305e5d5eb7c95ffc8e8a312e72a11fc
Harish Mohan Raj <harish@airt.ai>
2024-06-03 21:21:58 +0530

    Set FastAgency URL based on condition

--- app/src/server/common/constants.ts
+++ app/src/server/common/constants.ts
@@ -1,2 +1,4 @@
 const isDevEnv = process.env.NODE_ENV !== 'production';
-export const FASTAGENCY_SERVER_URL = isDevEnv ? 'http://127.0.0.1:8000' : 'https://api.staging.fastagency.ai';
+export const FASTAGENCY_SERVER_URL = isDevEnv
+  ? 'http://127.0.0.1:8000'
+  : process.env.FASTAGENCY_SERVER_URL || 'https://api.fastagency.ai';
